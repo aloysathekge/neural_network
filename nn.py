cd1a3644bfe26e6f04bd718e3664471551bda033
aloysathekge <sathekgealoy@gmail.com>
2024-08-10 11:51:11 +0200

    ReLU activation

--- nn.py
+++ nn.py
@@ -2,28 +2,35 @@
 import numpy as np
 import nnfs
 from nnfs.datasets import spiral_data
+
+nnfs.init()
+X, y = spiral_data(samples=100, classes=3)
+
 class Layer_Dense:
    
-    nnfs.init()
     def __init__(self, n_inputs, n_neurons):
     # Initialize weights and biases
-      
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
         print( "Weights are \n")
         print( self.weights)
-     
     # Forward pass
     def forward(self, inputs):
-    # Calculate output values from inputs, weights and biases
-        self.output = np.dot(inputs, self.weights) + self.biases
+        self.output= np.dot( inputs,self.weights)+ self.biases
+class Activation_Relu:
+    def forward(self, inputs):
+        self.output=np.maximum(0, inputs)
+
+layer= Layer_Dense(2,3)
+activation1=Activation_Relu()
+
+layer.forward(X)
+#print(layer.output)
+activation1.forward(layer.output)
+print("Activated Neurons are:\n")
+print(activation1.output)
 
 
-X, y = spiral_data(samples=100, classes=3)
-# Create Dense layer with 2 input features and 3 output values
-dense1 = Layer_Dense(2, 3)
-# Perform a forward pass of our training data through this layer
-dense1.forward(X)
-# Let's see output of the first few samples:
-print( "outputs are \n")
-print(dense1.output[:5])
+ 
+
+
