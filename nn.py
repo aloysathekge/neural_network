30c74fadc8d9673f1d191f916f11e655cbfd08d7
aloysathekge <sathekgealoy@gmail.com>
2024-08-10 13:14:39 +0200

    Softmax activation function

--- nn.py
+++ nn.py
@@ -12,23 +12,33 @@ class Layer_Dense:
     # Initialize weights and biases
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
-        print( "Weights are \n")
-        print( self.weights)
     # Forward pass
     def forward(self, inputs):
         self.output= np.dot( inputs,self.weights)+ self.biases
 class Activation_Relu:
     def forward(self, inputs):
         self.output=np.maximum(0, inputs)
+class Activation_Softmax:
+    def forward(self,inputs):
+        exp_values=np.exp(inputs-np.max(inputs, axis=0, keepdims=True))
+        probabilities=exp_values/np.sum(exp_values, axis=0, keepdims=True)
+        self.output=probabilities
 
-layer= Layer_Dense(2,3)
+layer1 =Layer_Dense(2,3)
 activation1=Activation_Relu()
 
-layer.forward(X)
+layer2= Layer_Dense(3,3)
+
+activation2=Activation_Softmax()
+
+layer1.forward(X)
 #print(layer.output)
-activation1.forward(layer.output)
-print("Activated Neurons are:\n")
-print(activation1.output)
+activation1.forward(layer1.output)
+layer2.forward(layer1.output)
+activation2.forward(layer2.output)
+
+print(activation2.output[:5])
+
 
 
  
